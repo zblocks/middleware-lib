673b6b89907b75d045311ed025135961fa085df9
sourabhmandal <19mandal97@gmail.com>
2022-09-01 11:08:45 +0530

    added mock functions and their tests and remove depricated functions

---
+++ main_test.go
@@ -0,0 +1,42 @@
+package middleware_test
+
+import (
+	"testing"
+
+	"github.com/Zbyteio/middleware-lib"
+	middlewareMock "github.com/Zbyteio/middleware-lib/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMockFunctions(t *testing.T)()  {
+  middleware.MiddlewareHandler = middlewareMock.MiddlewareMock
+	jwtToken := "test_jwt_string"
+	ctx := &gin.Context{}
+	eng := &gin.Engine{}
+	auth_base_url := "auth_base_url"
+	test_email := "test@gmail.com"
+	middlewareMock.MiddlewareMock.On("VerifyJwtTokenV2", ctx, jwtToken).Return(true)
+	middlewareMock.MiddlewareMock.On("SetCors", eng).Return(true)
+	middlewareMock.MiddlewareMock.On("GetUserID", auth_base_url, test_email).Return(middleware.GetUserIDResponse{
+		Status: true,
+		UserId: 80,
+	})
+
+
+
+	t.Run("When verify token mock function given correct data", func (t *testing.T)  {
+		resp := middlewareMock.MiddlewareMock.VerifyJwtTokenV2(ctx, jwtToken)
+		assert.Equal(t, true, resp)
+	})
+
+	t.Run("When SetCors() function given correct data", func (t *testing.T)  {
+		middlewareMock.MiddlewareMock.SetCors(eng)
+	})
+
+	t.Run("When GetUserID() function given correct data", func (t *testing.T)  {
+		resp := middlewareMock.MiddlewareMock.GetUserID(auth_base_url, test_email)
+		assert.Equal(t, true, resp.Status)
+		assert.Equal(t, 80, resp.UserId)
+	})
+}
--- middleware.go
+++ middleware.go
@@ -3,15 +3,12 @@ package middleware
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
-	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
 )
 
 var MiddlewareHandler middlewareInterface = &middlewareStruct{}
@@ -19,37 +16,11 @@ var MiddlewareHandler middlewareInterface = &middlewareStruct{}
 type middlewareStruct struct{}
 
 type middlewareInterface interface {
-	VerifyJwtToken(*gin.Context, string) (bool, jwt.MapClaims, int, error)
 	SetCors(*gin.Engine)
 	GetUserID(baseUrl string, userEmail string) GetUserIDResponse
 	VerifyJwtTokenV2(c *gin.Context, authServiceBaseUrl string) bool
 }
 
-func (m *middlewareStruct) VerifyJwtToken(c *gin.Context, jwtSecret string) (bool, jwt.MapClaims, int, error) {
-	auth_token := c.Request.Header["Authorization"]
-
-	// no auth token error
-	if len(auth_token) == 0 {
-		return false, nil, ErrAuthorizationTokenEmpty, errors.New(AuthorizationTokenEmpty)
-	}
-
-	jwttoken := strings.Split(auth_token[0], " ")[1]
-
-	claims := jwt.MapClaims{}
-	tkn, err := jwt.ParseWithClaims(jwttoken, claims, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New(AuthorizationTokenInvalid)
-		}
-		return []byte(jwtSecret), nil
-	})
-	if err != nil {
-		return false, nil, ErrAuthorizationTokenInvalid, errors.New(AuthorizationTokenInvalid)
-	}
-	if !tkn.Valid {
-		return false, nil, ErrAuthorizationTokenInvalid, errors.New(AuthorizationTokenInvalid)
-	}
-	return true, claims, 0, nil
-}
 
 func (m *middlewareStruct) SetCors(r *gin.Engine) {
 	// set cors access
