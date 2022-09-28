83b67c0f94a5f689a76586ef261474d3cd00682c
sourabhmandal <19mandal97@gmail.com>
2022-09-28 19:21:08 +0530

    add logging and failsafe url return

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -38,58 +39,83 @@ func (m *middlewareStruct) SetCors(r *gin.Engine) {
 
 func (m *middlewareStruct) GetUserJwtData(userServiceBaseUrl string, userEmail string) GetUserDataByEmailResponse {
 	api, _ := url.JoinPath(userServiceBaseUrl, "/getUserProfileByEmail")
+	log.Printf(`USER SERVICE BASE URL :: %s / USER EMAIL :: %s / CALLING API :: %s`, userServiceBaseUrl, userEmail, api)
 	body := []byte(fmt.Sprintf(`{"userEmail": "%s"}`, userEmail))
 
 	r, err := http.NewRequest("POST", api, bytes.NewBuffer(body))
 	if err != nil {
-		panic(err)
+		log.Printf("CREATE API REQUEST ERROR :: %v", err)
+		return GetUserDataByEmailResponse{
+			Status: false,
+		}
 	}
 	r.Header.Add("Content-Type", "application/json")
 
 	client := &http.Client{}
 	res, err := client.Do(r)
+	log.Printf("CALLED API :: %s", api)
 	if err != nil {
-		panic(err)
+		log.Printf("RECIEVED API ERROR :: %v", err)
 	}
 
 	defer res.Body.Close()
 
 	var data GetUserDataByEmailResponse
+	log.Println("parsing json response")
 	derr := json.NewDecoder(res.Body).Decode(&data)
 	if derr != nil {
-		panic(derr.Error())
+		log.Printf("JSON PARSER ERROR :: %v", derr)
+		return GetUserDataByEmailResponse{
+			Status: false,
+		}
 	}
 
 	if data.Status {
 		return data
 	}
-	return GetUserDataByEmailResponse{}
+	return GetUserDataByEmailResponse{
+		Status: false,
+	}
 }
 
 func (m *middlewareStruct) VerifyJwtToken(c *gin.Context, authServiceBaseUrl string) *ValidJwt {
 	api, _ := url.JoinPath(authServiceBaseUrl, "/v1/auth/verifyToken")
+	log.Printf(`AUTH SERVICE BASE URL :: %s / CALLING API %s`, authServiceBaseUrl, api)
 	r, err := http.NewRequest("GET", api, nil)
 	if err != nil {
-		panic(err)
+		log.Printf("CREATE API REQUEST ERROR :: %v", err)
+		return &ValidJwt{
+			IsValid: false,
+		}
 	}
 	r.Header.Add("Content-Type", "application/json")
 	r.Header.Add("Authorization", c.Request.Header["Authorization"][0])
 
 	client := &http.Client{}
 	res, err := client.Do(r)
+	log.Printf("CALLED API :: %s", api)
 	if err != nil {
-		panic(err)
+		log.Printf("RECIEVED API ERROR :: %v", err)
+		return &ValidJwt{
+			IsValid: false,
+		}
 	}
 
 	defer res.Body.Close()
 
 	var data VerifyJwtTokenResponse
+	log.Println("parsing json response")
 	derr := json.NewDecoder(res.Body).Decode(&data)
 	if derr != nil {
-		panic(derr.Error())
+		log.Printf("JSON PARSER ERROR :: %v", derr)
+		return &ValidJwt{
+			IsValid: false,
+		}
 	}
 	if data.Status {
 		return data.Data
 	}
-	return nil
+	return &ValidJwt{
+		IsValid: false,
+	}
 }
